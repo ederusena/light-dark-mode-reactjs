9f2ec313c77811cdef92da3067f8ac3b9c1b3841
Eder Sena <eder.sena@live.com>
2022-04-15 15:33:48 -0300

    🔧: adding ThemeContext

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-// import styles from '../styles/Home.module.css'
-
 import Form from "../components/Form";
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext(null);
 
 export default function Home() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((theme) => (theme === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="App">
-      <Form />
-    </div>
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      <div className="App" id={theme}>
+        <Form />
+      </div>
+    </ThemeContext.Provider>
   )
 }
