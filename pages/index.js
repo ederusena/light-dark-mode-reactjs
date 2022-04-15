800cd5b90e0ac47cb5bb20961d613fb4d789629b
Eder Sena <eder.sena@live.com>
2022-04-15 15:41:06 -0300

    ::white_check_mark: Done

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Form from "../components/Form";
 import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
 
 export const ThemeContext = createContext(null);
 
@@ -14,6 +15,10 @@ export default function Home() {
     <ThemeContext.Provider value={{theme, toggleTheme}}>
       <div className="App" id={theme}>
         <Form />
+        <div className="switch">
+          <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+          <ReactSwitch onChange={toggleTheme} checked={theme === 'dark'}/>
+        </div>
       </div>
     </ThemeContext.Provider>
   )
